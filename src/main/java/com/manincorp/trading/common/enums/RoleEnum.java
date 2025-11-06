5ba44a471befb151babdd8d0c28819354f8123ef
1579923417 <1579923417@qq.com>
2025-11-06 13:36:51 +0800

    feat: add RoleEnum for user roles administration

---
+++ src/main/java/com/manincorp/trading/common/enums/RoleEnum.java
@@ -0,0 +1,12 @@
+package com.manincorp.trading.common.enums;
+
+/**
+ * author: Jamie
+ * Package: com.manincorp.trading.common.enums.RoleEnum
+ * Date: 2025-11-06 13:36
+ * Description:
+ */
+public enum RoleEnum {
+    ADMIN,
+    USER
+}

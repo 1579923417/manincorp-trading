582ad028cba5c77164d3daf122612dade822e99e
1579923417 <1579923417@qq.com>
2025-11-11 11:17:11 +0800

    feat: add wang-editor file upload endpoint and update file handling

--- src/main/java/com/manincorp/trading/controller/FileController.java
+++ src/main/java/com/manincorp/trading/controller/FileController.java
@@ -1,6 +1,8 @@
 package com.manincorp.trading.controller;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Dict;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.StrUtil;
 import com.manincorp.trading.common.Result;
@@ -14,6 +16,8 @@ import java.io.File;
 import java.io.OutputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * author: Jamie
@@ -25,7 +29,7 @@ import java.nio.charset.StandardCharsets;
 @RequestMapping("/files")
 public class FileController {
 
-    private static final String filePath = System.getProperty("user.dir") + "/files";
+    private static final String filePath = System.getProperty("user.dir") + "/files/";
 
     @Value("${file.port}")
     private String port;
@@ -52,9 +56,7 @@ public class FileController {
             if (!FileUtil.isDirectory(filePath)) {
                 FileUtil.mkdir(filePath);
             }
-            // 文件存储形式：时间戳-文件名
-            String filePathName = flag + "-" + fileName;
-            FileUtil.writeBytes(file.getBytes(), filePath + "/" + filePathName);  //ps: ***/manager/files/1697438073596-avatar.png
+            FileUtil.writeBytes(file.getBytes(), filePath + flag + "-" + fileName);  // ***/manager/files/1697438073596-avatar.png
             System.out.println(fileName + "--上传成功");
         } catch (Exception e) {
             System.err.println(fileName + "--文件上传失败");
@@ -66,10 +68,10 @@ public class FileController {
 
 
     /**
-     * download file
+     *  file
      */
-    @GetMapping("/download")   // ps: 1697438073596-avatar.png
-    public void avatarPath(@RequestParam String flag, HttpServletResponse response) {
+    @GetMapping("/{flag}")   //  1697438073596-avatar.png
+    public void avatarPath(@PathVariable String flag, HttpServletResponse response) {
         OutputStream os;
         try {
             if (StrUtil.isNotEmpty(flag)) {
@@ -104,5 +106,27 @@ public class FileController {
             System.err.println("删除文件 " + flag + " 时发生异常: " + e.getMessage());
             return Result.error("删除图片失败", e.toString());
         }
+    }
+
+    /**
+     * wang-editor upload file
+     */
+    @PostMapping("/wang/upload")
+    public Map<String, Object> wangEditorUpload(MultipartFile file) {
+        String flag = System.currentTimeMillis() + "";
+        String fileName = file.getOriginalFilename();
+        try {
+            // 文件存储形式：时间戳-文件名
+            FileUtil.writeBytes(file.getBytes(), filePath + flag + "-" + fileName);
+            System.out.println(fileName + "--上传成功");
+            Thread.sleep(1L);
+        } catch (Exception e) {
+            System.err.println(fileName + "--文件上传失败");
+        }
+        String http = "http://" + ip + ":" + port + "/files/";
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("errno", 0);
+        resMap.put("data", CollUtil.newArrayList(Dict.create().set("url", http + flag + "-" + fileName)));
+        return resMap;
     }
 }
